4c4badd2b611e0e6641f8a95f2ae8fded75b2316
Dan O'Hearn <Dan.OHearn@vu.com>
2022-02-17 10:50:50 -0600

    moved print statement

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,9 +1,26 @@
 def calculateGrade():
     # Implement your solution in between the two comment blocks
     print("Calculating Grade")
-    # This first line is provided for you
+    # This first line is provided for you    
 
-    hrs = float(input("Enter score:"))
+    try:
+        x = float(input("Enter score: "))
+
+        if x >= 0.9 :   
+            print("A")
+        elif x >= 0.8 : 
+            print("B")
+        elif x >= 0.7 :  
+            print("C")
+        elif x >= 0.6 :
+            print("D")
+        elif x < 0.6 :
+            print("F")
+        else :
+            print("Bad Score")
+    except:
+        print('Bad Score')
+
 
     # end assignment
 
